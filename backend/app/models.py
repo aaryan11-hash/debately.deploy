4a22e930a11162500ad9cafd922702aeb7df4286
aaryan11-hash <aaryan2000dpspune@gmail.com>
2021-05-29 22:09:01 +0530

    make it better

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from fastapi.params import File
+from pydantic import BaseModel
+
+class Profile(BaseModel):
+    email:str
+    name : str
+    wallet_amt : int
+    rating : int
+    total_debates_attended : int
+    
+class Report(BaseModel):
+    reportForUser1 : str
+    reportForUser2 : str
+
+
+class SessionDetails(BaseModel):
+    session_id : int
+    anonymous_name_1 : str
+    anonymous_name_2 : str
+    user_1_email : str
+    user_2_email : str
+    report : Report
+    
+
+
+
+
---
+++ backend/app/profilecontroller.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter
+import json
+from bson import json_util
+# from ..configs.mongoconnect import connectMongoClient
+from .models import Profile
+from .configs.mongoconnect import connectMongoClient
+
+router = APIRouter(prefix="/profiles")  
+
+db = connectMongoClient()
+collection = db['test']
+
+@router.get('/')
+def getProfiles():
+    
+    """Gets all profile details
+
+    Parameters:
+    None
+
+    Returns:
+    Json with Profiles"""
+    
+    profile_list = []
+    profile_results = collection.find()
+
+    for profiles in profile_results:
+        profile_list.append(json.loads(json_util.dumps(profiles)))
+    
+    return profile_list
+
+
+@router.get('/{profile_email}')
+def getProfileByProfileId(profile_email : str):
+    """Gets all profile details of user with given profile_email
+
+    Parameters:
+    str: profile_email
+
+    Returns:
+    Json with Profile details """
+
+    profile_result = collection.find_one({"email":profile_email})
+
+    profile = json.loads(json_util.dumps(profile_result))
+    
+    return profile
+
+@router.post('/')
+def postNewProfile(profile : Profile):
+    
+    """Gets all profile details of user with given profile_email
+
+    Parameters:
+    str: profile_email
+
+    Returns:
+    Json with Profile details """
+    
+    profile_email = profile.email
+    profile_query = collection.find({"email":profile_email})
+    profile_query = [item for item in profile_query]
+
+    if not profile_query : 
+        collection.save(dict(profile))
+        return True
+
+    return False
+
+@router.delete('/{profile_email}',status_code=200)
+def deleteProfileById(profile_email : str):
+    """Deletes all profile details of user with given profile_email
+
+    Parameters:
+    str: profile_email
+
+    Returns:
+    Bool: Success of deletion """
+    
+    result = collection.delete_one({"email":profile_email})
+    
+    return result.acknowledged
